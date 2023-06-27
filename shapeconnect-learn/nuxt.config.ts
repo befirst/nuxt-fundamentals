e38d675ef891dcf042d6a4c76bb8a811f11d8de3
Denis Arkhireev <den-uln@mail.ru>
2023-06-27 15:19:01 +0400

    Added default Chivo font

--- shapeconnect-learn/nuxt.config.ts
+++ shapeconnect-learn/nuxt.config.ts
@@ -3,11 +3,23 @@ import daisyui from 'daisyui'
 
 export default defineNuxtConfig({
     devtools: { enabled: true },
+    css: ['~/assets/css/main.css'],
     modules: [
         '@nuxtjs/eslint-module',
+        '@nuxtjs/google-fonts',
         '@nuxtjs/tailwindcss',
         'nuxt-typed-router',
     ],
+    googleFonts: {
+        useStylesheet: true,
+        prefetch: true,
+        preconnect: true,
+        preload: true,
+        download: true,
+        families: {
+            Chivo: true,
+        },
+    },
     tailwindcss: {
         config: {
             content: [],
@@ -15,7 +27,6 @@ export default defineNuxtConfig({
         },
     },
     typescript: {
-        typeCheck: true,
         shim: false,
         strict: true,
     },
